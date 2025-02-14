d9a0d7b505b525c40e2557fc368cfc7d6fafe7ad
Kalil Teotonio <80278976+Galill@users.noreply.github.com>
2025-02-13 23:13:38 -0300

    Add files via upload

---
+++ Atividade 04/main (1).cpp
@@ -0,0 +1,75 @@
+//compilador ECI (Expressões )
+//#include <stack>
+#include <iostream>
+#include <fstream>
+#include <ostream>
+#include <string>
+#include <map>
+#include <vector>
+
+struct token
+{
+    std::string tipo;
+    std::string lexema;
+    int posicao;
+};
+
+bool is_number(const std::string& s)
+{
+    std::string::const_iterator it = s.begin();
+    while (it != s.end() && std::isdigit(*it)) ++it;
+    return !s.empty() && it == s.end();
+}
+
+
+int main(int argc, char const *argv[])
+{
+    std::string line;
+    std::ifstream file;
+    std::map <std::string, std::string> dicionario;
+
+    dicionario["+"] = "Soma";
+    dicionario["-"] = "Subtracao";
+    dicionario["*"] = "Multiplicacao";
+    dicionario["/"] = "Divisao";
+    dicionario["("] = "ParenEsq";
+    dicionario[")"] = "ParenDir";
+    dicionario[" "] = "Vazio";
+
+    file.open("bomba.eci");
+    if(!file.is_open()){
+        std::cout << "Não foi possivel abrir o arquivo" << std::endl; 
+        exit(1);
+    }
+    
+    std::getline(file,line);
+    token Tokens[100];
+    std::string fe;
+    int ContadorToken = 0;
+    for(int i = 0; i < line.size(); ++i){
+        
+        try{
+            std::string s;
+            s.push_back(line[i]);
+            std::cout << dicionario.at(s) << std::endl;
+            std::cout << fe;
+            if(fe != ""){
+                Tokens[ContadorToken].tipo = "Numero";
+                Tokens[ContadorToken].lexema = fe;
+                Tokens[ContadorToken].posicao = i-1;
+                ContadorToken++;
+            }
+            
+            fe = "";
+        }
+        catch(const std::out_of_range& e){
+            fe.push_back(line[i]);
+        }
+    }
+    
+    std::cout << Tokens[1].tipo;
+    std::cout << Tokens[1].lexema;
+    std::cout << Tokens[1].posicao;
+    std::cout << line;
+    return 0;
+}
